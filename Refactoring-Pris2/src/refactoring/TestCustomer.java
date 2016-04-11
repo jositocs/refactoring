55a78a64fab765440240e7606311b6e283e179c9
jose manuel lopez <joselitolg89@gmail.com>
2016-04-11 17:24:27 +0200

    Extract method en el metodo statement en la parte de frequentRenterPointer

--- Refactoring-Pris2/src/refactoring/TestCustomer.java
+++ Refactoring-Pris2/src/refactoring/TestCustomer.java
@@ -8,9 +8,9 @@ public class TestCustomer {
 	@Test
 	public void test() {
 		//PELICULAS DE ALQUILER
-		Movie movie1=new Movie("La sirenita",2);
-		Movie movie2=new Movie("Origen",0);
-		Movie movie3=new Movie("Batman v Superman",1);
+		Movie movie1=new Movie("Titanic",2);
+		Movie movie2=new Movie("Diario de Noa",0);
+		Movie movie3=new Movie("Sherlock Holmes",1);
 		
 		//ALQUILERES
 		Rental rental1=new Rental(movie1,4);
@@ -18,16 +18,16 @@ public class TestCustomer {
 		Rental rental3=new Rental(movie3,2);
 		
 		//CLIENTES
-		Customer customer1=new Customer("name1");
-		Customer customer2=new Customer("name2");
+		Customer customer1=new Customer("Jose");
+		Customer customer2=new Customer("Gema");
 		
 		customer1.addRental(rental1);
 		customer2.addRental(rental2);
 		customer2.addRental(rental3);
 		
 		
-		assertEquals(customer2.statement(),"Rental Record for name2"+"\n"+"	Origen	2.0"+"\n"+"	Batman v Superman	6.0"+"\n"+"Amount owed is 8.0"+"\n"+"You earned 3 frequent renter points");
-		assertEquals(customer1.statement(),"Rental Record for name1"+"\n"+"	La sirenita	3.0"+"\n"+"Amount owed is 3.0"+"\n"+"You earned 1 frequent renter points");
+		assertEquals(customer2.statement(),"Rental Record for Jose"+"\n"+"	Titanic"+"\n"+"	Diario de Noa	6.0"+"\n"+"Amount owed is 8.0"+"\n"+"You earned 3 frequent renter points");
+		assertEquals(customer1.statement(),"Rental Record for Gema"+"\n"+"	Titanic	3.0"+"\n"+"Amount owed is 3.0"+"\n"+"You earned 1 frequent renter points");
 	}
 
 }
